92c1df307b9bf67c767a0c6df9e758c2129823f4
Ayush Dixit <117524157+ayushdixit23@users.noreply.github.com>
2023-12-19 00:37:17 +0530

    Update Fetch.jsx
    "changed"

--- app/com/Fetch.jsx
+++ app/com/Fetch.jsx
@@ -1,62 +1,77 @@
-"use client";
 import React from "react";
-import Image from "next/image";
-import { AiOutlineReload } from "react-icons/ai";
-const Fetch = ({ load, data }) => {
+
+const Pagination = ({ length, postPerPage, currentPage, setCurrentPage }) => {
+  let page = [];
+  const totalPages = Math.ceil(length / postPerPage);
+
+  // Function to generate an array of page numbers
+  const generatePageArray = () => {
+    const maxButtons = 5;
+    const maxPagesToShow = Math.min(totalPages, maxButtons);
+    const middleIndex = Math.ceil(maxPagesToShow / 2);
+
+    if (currentPage <= middleIndex) {
+      for (let i = 1; i <= maxPagesToShow; i++) {
+        page.push(i);
+      }
+    } else if (currentPage > totalPages - middleIndex) {
+      for (let i = totalPages - maxPagesToShow + 1; i <= totalPages; i++) {
+        page.push(i);
+      }
+    } else {
+      for (
+        let i = currentPage - Math.floor(maxPagesToShow / 2);
+        i <= currentPage + Math.floor(maxPagesToShow / 2);
+        i++
+      ) {
+        page.push(i);
+      }
+    }
+  };
+
+  generatePageArray();
+
   return (
-    <>
-      {load ? (
-        <div className="overflow-hidden">
+    <div className="flex justify-center items-center gap-3">
+      <button
+        onClick={() => setCurrentPage((prevPage) => prevPage - 1)}
+        disabled={currentPage === 1}
+        className="text-white bg-black rounded-lg p-3"
+      >
+        Previous
+      </button>
+
+      <div className="flex justify-center items-center my-5">
+        {page.map((p, i) => (
           <div
-            className="animate-spin flex h-[100vh] justify-center
-		 items-center"
+            key={i}
+            onClick={() => setCurrentPage(p)}
+            className={`text-white gap-2 rounded-md p-3 px-6 ${
+              p === currentPage
+                ? "bg-black shadow-2xl border-2"
+                : "bg-blue-700 border"
+            }`}
           >
-            {" "}
-            <AiOutlineReload className="text-xl text-black" />
+            {p}
           </div>
-        </div>
-      ) : (
-        <div className="flex justify-center items-center ">
-          <div className="grid grid-cols-3 gap-7 w-full px-[3%] py-3">
-            {data.map((d, i) => (
-              <div
-                key={d.id}
-                className="border-2 p-[2%] flex-1 basis-full h-auto rounded-xl border-black"
-              >
-                <div className="flex justify-center rounded-xl items-center">
-                  <Image
-                    src={d?.image}
-                    alt={d.title}
-                    width={200}
-                    height={200}
-                    className="max-h-[250px] max-w-[250px] min-w-[250px] min-h-[250px]"
-                  />
-                </div>
-                <div className="flex flex-col w-full justify-center px-2 items-center">
-                  <div className="py-2 font-semibold text-3xl">
-                    {d.category.slice(0, 15)}
-                  </div>
-                  <div className="py-2 font-medium">{`${d.description.slice(
-                    0,
-                    80
-                  )}...`}</div>
-                  <div className="flex justify-between items-center pb-2 w-full">
-                    <div className="font-medium text-xl text-[#4b80cb]">
-                      <span className="text-black font-semibold">Price :</span>{" "}
-                      $ {d.price}
-                    </div>
-                    <div className="p-3 px-4 rounded-md bg-black text-white">
-                      Add to Cart
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
+        ))}
+
+        {totalPages > 5 && currentPage + Math.floor(5 / 2) < totalPages && (
+          <div className="text-black gap-2 rounded-md p-3 px-6 cursor-not-allowed">
+            ...
           </div>
-        </div>
-      )}
-    </>
+        )}
+      </div>
+
+      <button
+        onClick={() => setCurrentPage((prevPage) => prevPage + 1)}
+        disabled={currentPage === totalPages}
+        className="text-white bg-black rounded-lg p-3 px-6"
+      >
+        Next
+      </button>
+    </div>
   );
 };
 
-export default Fetch;
+export default Pagination;
